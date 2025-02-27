8d36419d2c939e4a7f71d54c133dcf65ee10e8a3
Sourav <>
2025-02-27 13:20:29 -0500

    view updated

---
+++ SafeCircle/SafeCircle/Extension/UIViewExtension.swift
@@ -0,0 +1,45 @@
+//
+//  Extension.swift
+//
+//
+//  Created by Soarov Chakra Borty on 2/27/25.
+//
+
+import UIKit
+extension UIView {
+    func roundTopCorners(_ radius: CGFloat = 10) {
+
+        self.clipsToBounds = true
+        self.layer.cornerRadius = radius
+        if #available(iOS 11.0, *) {
+            self.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        } else {
+            self.roundCorners(corners: [.topLeft, .topRight], radius: radius)
+        }
+    }
+
+    func roundBottomCorners(_ radius: CGFloat = 10) {
+
+        self.clipsToBounds = true
+        self.layer.cornerRadius = radius
+        if #available(iOS 11.0, *) {
+            self.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        } else {
+            self.roundCorners(corners: [.bottomLeft, .bottomRight], radius: radius)
+        }
+    }
+
+    func roundBothCorner(_ radius: CGFloat = 10) {
+
+        self.clipsToBounds = true
+        self.layer.cornerRadius = radius
+    }
+
+    private func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+
+         let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+         let mask = CAShapeLayer()
+         mask.path = path.cgPath
+         layer.mask = mask
+     }
+}
--- SafeCircle/SafeCircle/ViewControllers/LandingViewController.swift
+++ SafeCircle/SafeCircle/ViewControllers/LandingViewController.swift
@@ -6,10 +6,14 @@ import UIKit
 
 class LandingViewController: UIViewController {
     
+    @IBOutlet weak var notificationView: UIView!
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
+        self.updateView()
     }
-    
+    private func updateView(){
+        self.notificationView.roundBothCorner(30.0)
+    }
 }
