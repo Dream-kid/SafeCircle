833c907f814963cf63da546474eb071f9949d18f
Sourav <>
2025-02-27 14:05:16 -0500

    landing storyboard updated

--- SafeCircle/SafeCircle/ViewControllers/LandingViewController.swift
+++ SafeCircle/SafeCircle/ViewControllers/LandingViewController.swift
@@ -7,7 +7,11 @@ import UIKit
 class LandingViewController: UIViewController {
     
     @IBOutlet weak var notificationView: UIView!
-    
+    @IBOutlet weak var manageProfile: UIView!
+    @IBOutlet weak var locationView: UIView!
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var patientView: UIView!
+    @IBOutlet weak var sosView: UIView!
     override func viewDidLoad() {
         
         super.viewDidLoad()
@@ -15,5 +19,11 @@ class LandingViewController: UIViewController {
     }
     private func updateView(){
         self.notificationView.roundBothCorner(30.0)
+        self.manageProfile.roundBothCorner(30.0)
+        self.locationView.roundBothCorner(30.0)
+        self.videoView.roundBothCorner(30.0)
+        self.sosView.roundBothCorner(30.0)
+        self.patientView.roundBothCorner(30.0)
+
     }
 }
