58e0cdf250642b2e13db50a85e0a6e4e79383ce7
Sourav <>
2025-03-03 15:56:46 -0500

    sign up added

--- SafeCircle/Defaults/SceneDelegate.swift
+++ SafeCircle/Defaults/SceneDelegate.swift
@@ -56,8 +56,8 @@ extension SceneDelegate {
     
     private func getInitialVC() -> UIViewController {
 
-        let landingStoryboard = UIStoryboard(name: "Login", bundle: nil)
-        let landingVC = landingStoryboard.instantiateViewController(withIdentifier: "LoginVC") as! LoginVC
+        let landingStoryboard = UIStoryboard(name: "Register", bundle: nil)
+        let landingVC = landingStoryboard.instantiateViewController(withIdentifier: "RegisterVC") as! RegisterVC
         let navController = UINavigationController(rootViewController: landingVC)
         navController.navigationBar.isHidden = true
         return navController
