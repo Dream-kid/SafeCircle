841547e5437e276b84f5a66f160d02c160898e1a
Sourav <>
2025-03-05 13:21:08 -0500

    video list storyboard added

--- SafeCircle/Defaults/SceneDelegate.swift
+++ SafeCircle/Defaults/SceneDelegate.swift
@@ -56,8 +56,8 @@ extension SceneDelegate {
     
     private func getInitialVC() -> UIViewController {
 
-        let landingStoryboard = UIStoryboard(name: "EmergencyContacts", bundle: nil)
-        let landingVC = landingStoryboard.instantiateViewController(withIdentifier: "EmergencyContactsVC") as! EmergencyContactsVC
+        let landingStoryboard = UIStoryboard(name: "videoList", bundle: nil)
+        let landingVC = landingStoryboard.instantiateViewController(withIdentifier: "videoListVc") as! videoListVc
         let navController = UINavigationController(rootViewController: landingVC)
         navController.navigationBar.isHidden = true
         return navController
